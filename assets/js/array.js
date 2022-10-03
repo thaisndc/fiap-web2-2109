0bdd7edfae9d93143e6ad65d64dcc239f1b76aa1
Thais Nascimento <thais.12_@hotmail.com>
2022-10-03 14:12:28 -0300

    'Deploy'

---
+++ assets/js/array.js
@@ -0,0 +1,25 @@
+const carros = document.querySelector("#carros");
+//document.getElementById - sempre existiu...
+//document.querySelector - mais recente... permite usar seletores do ss3.
+const carrosArray = ["Chevrolet", "Fiat", "Ford", "Honda", "Hyundai", "Kia", "Mazda", "Mercedes-Benz", "Nissan", "Peugeot", "Renault", "Toyota", "Volkswagen"];
+
+carrosArray.forEach((value, key) => {
+    carros.innerHTML += `<option value="${key}">${value}</option>`;
+  });
+  
+  carrosArray.forEach((value) => console.log(value));
+  
+  const importados = [
+    ["BMW", "320i", "M3", "M4", "X5", "X6", "z4 m40i"],
+    ["Audi", "a4", "s4", "s4 avanti", "Q7", "RS7", "e-Tron"],
+    ["Mercedes", "gtr", "e63", "s63", "brabus gle", "AMG G63"],
+  ];
+  
+  importados.forEach((marcas) => {
+    const select = document.createElement("select");
+    select.setAttribute("id", marcas[0]);
+    marcas.forEach((carros, key) => {
+      select.innerHTML += `<option value="${carros}">${carros}</option>`;
+    });
+    document.querySelector("body").appendChild(select);
+  });
