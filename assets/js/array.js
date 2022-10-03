d56d50a1100978348632f7125691fc78e5dd06c0
Thais Nascimento <thais.12_@hotmail.com>
2022-10-03 15:56:16 -0300

    'Deploy'

--- assets/js/array.js
+++ assets/js/array.js
@@ -23,3 +23,90 @@ carrosArray.forEach((value, key) => {
     });
     document.querySelector("body").appendChild(select);
   });
+
+
+
+  const aereas = [
+    "Latam",
+    "Gol",
+    "Azul",
+    "Delta",
+    "American Airlines",
+    "Emirates",
+    "Qatar",
+  ];
+  
+  const comboAereas = document.createElement("select");
+  comboAereas.setAttribute("id", "aereas");
+  
+  aereas.forEach((aerea, key) => {
+    comboAereas.innerHTML += `<option value="${key}">${aerea}</option>`;
+  });
+  //adicione este combo como um filho no body, no final de todas as tags.
+  document.querySelector("body").appendChild(comboAereas);
+  const menu = [
+    "Todos",
+    "Venda na Amazon",
+    "Ofertas do Dia",
+    "Prime",
+    "Eletrônicos",
+    "Ideias para Presente",
+    "Brinquedos e Jogos",
+    "Computadores",
+    "eBooks Kindle",
+    "Games",
+    "Ferramentas e Construção",
+    "Livros",
+  ];
+  
+  const ulMenu = document.createElement("ul");
+  ulMenu.setAttribute("id", "menu");
+  ulMenu.setAttribute("class", "menu-site");
+  
+  menu.forEach((value, index) => {
+    ulMenu.innerHTML += `<li>${value}</li>`;
+  });
+  
+  document.querySelector("nav").appendChild(ulMenu);
+
+
+
+
+
+  const const carros = ["Corsa", "Uno", "Gol", "Palio", "Kombi", "Peugeot 308"];
+
+//adicionar um elemento NO FINAL do array. Push
+newCarros.push("Celta", "Monza");
+
+//adicionar um elemento NO INÍCIO do array. Unshift
+newCarros.unshift("Fusca");
+
+//remover um elemento do FINAL do array. Pop
+const ultimo = newCarros.pop(); //retorna o elemento removido
+
+const primeiro = newCarros.shift(); //retorna o elemento removido
+
+const nomeCompleto = "Thais Nascimento da Costa";
+const nomeArray = nomeCompleto.split(" ");
+const americanName = [nomeArray.at(-1), nomeArray[0]].join(", ");
+// THAIS N DA COSTA
+// COSTA, THAIS
+
+
+const removeUm = newCarros.splice(0, 3);
+
+console.log(removeUm, "Remove um, splice");
+console.log(americanName, "Formato americano");
+console.log(newCarros, "NewCarros");
+
+//push, pop
+//shift, unshift
+//splice
+//split
+//join
+
+// Programação funcional
+//slice
+//map
+//filter
+//find
